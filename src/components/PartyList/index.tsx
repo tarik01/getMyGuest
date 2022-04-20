7e11e65cbdd4df87457e83fc018007e4c73dae52
Tarik Nazorek <jantarik@gmail.com>
2022-04-20 16:45:03 -0300

    handling errors in PartyList component

--- src/components/PartyList/index.tsx
+++ src/components/PartyList/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getParties } from '../../services/useParty';
 import { Party } from '../../types';
+import Item404 from '../Item404';
 import PartyItem from '../PartyItem';
 import SearchAndAddBar from '../SearchAndAddBar';
 import { List } from './styles';
@@ -8,6 +9,7 @@ import { List } from './styles';
 export default function PartyList() {
   const [parties, setParties] = useState([]);
   const [search, setSearch] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,7 +17,9 @@ export default function PartyList() {
       setParties(partiesData);
     };
     // eslint-disable-next-line no-console
-    fetchData().catch(console.error);
+    const result = fetchData().catch(() => {
+      setError('Não foi possível obter os dados, tente novamente!');
+    });
   }, []);
 
   return (
@@ -24,22 +28,29 @@ export default function PartyList() {
         searchPlaceholder="Pesquisar Evento"
         imageAddPlaceholder="Novo Evento"
       />
-      <List>
-        {parties.map((party: Party) => (
-          <li key={party.id}>
-            <PartyItem
-              id={1}
-              name={party.name}
-              description={party.description}
-              date={party.date}
-              time={party.time}
-              image={party.image}
-              location={party.location}
-              actionButton
-            />
-          </li>
-        ))}
-      </List>
+      {error !== '' && (
+        <Item404>
+          <h1>{error}</h1>
+        </Item404>
+      )}
+      {error === '' && parties.length > 0 && (
+        <List>
+          {parties.map((party: Party) => (
+            <li key={party.id}>
+              <PartyItem
+                id={1}
+                name={party.name}
+                description={party.description}
+                date={party.date}
+                time={party.time}
+                image={party.image}
+                location={party.location}
+                actionButton
+              />
+            </li>
+          ))}
+        </List>
+      )}
     </div>
   );
 }
