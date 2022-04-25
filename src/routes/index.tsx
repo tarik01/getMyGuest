0f2668c5aa85818c001a24524dc95ff37cff9f76
Tarik Nazorek <jantarik@gmail.com>
2022-04-25 08:54:37 -0300

    adding login and register into routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import Login from '../templates/Login';
+import Register from '../templates/Register';
 import MyAccount from '../templates/MyAccount';
 import Parties from '../templates/Parties';
 import Guests from '../templates/Guests';
@@ -8,6 +10,8 @@ import PartyPage from '../templates/PartyPage';
 export default function GlobalRoutes() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/parties" element={<Parties />} />
       <Route path="/parties/:id" element={<PartyPage />} />
       <Route path="/guests" element={<Guests />} />
