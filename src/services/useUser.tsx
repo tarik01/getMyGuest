4476135dda332a15da03118f3123868c5362fc82
Tarik Nazorek <jantarik@gmail.com>
2022-05-02 22:12:36 -0300

    Improving the api services

--- src/services/useUser.tsx
+++ src/services/useUser.tsx
@@ -1,3 +1,4 @@
+import { isJsonString } from '../util/util-fuctions';
 import { API_URL } from './api';
 
 export async function registerUser({
@@ -14,7 +15,7 @@ export async function registerUser({
     password,
     confirmPassword,
   };
-  const parties = await fetch(`${API_URL}/api/users/register/`, {
+  const register = await fetch(`${API_URL}/api/users/register/`, {
     method: 'POST',
     body: JSON.stringify(user),
     headers: {
@@ -32,5 +33,45 @@ export async function registerUser({
     .catch(error => {
       throw new Error(error.message);
     });
-  return parties;
+  return register;
+}
+
+export async function LoginUser({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = {
+    username: email,
+    password,
+  };
+  const login = await fetch(`${API_URL}/api/users/auth/`, {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return res.text().then(text => {
+        throw new Error(text);
+      });
+    })
+    .catch(err => {
+      if (isJsonString(err.message)) {
+        const errorsFromApi = JSON.parse(err.message);
+        if ('non_field_errors' in errorsFromApi) {
+          throw new Error(errorsFromApi.non_field_errors[0]);
+        }
+      }
+      throw new Error(
+        'Não foi possível executar a requisição, tente novamente!',
+      );
+    });
+  return login;
 }
